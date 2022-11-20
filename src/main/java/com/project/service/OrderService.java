695293e90ac75e9f19479273206543c855aca3ac
Artem <kurilko365@gmail.com>
2022-11-20 14:33:42 +0200

    fix: bugs

--- src/main/java/com/project/service/OrderService.java
+++ src/main/java/com/project/service/OrderService.java
@@ -2,16 +2,11 @@ package com.project.service;
 
 import com.project.model.Currencies;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.http.client.utils.URIBuilder;
+import okhttp3.*;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.ZoneId;
@@ -27,10 +22,10 @@ public class OrderService {
     private static final String CURRENCY_PAIR = "BTCUSDT";
     private static final float BUY_PRICE_COEFFICIENT = 0.99f;
     private static final float SELL_PRICE_COEFFICIENT = 1.01f;
-    private static final HttpClient client = HttpClient.newHttpClient();
-    private static HttpRequest request;
+    private static final OkHttpClient client = new OkHttpClient();
+    private static Request request;
 
-    public static void placeOrder(boolean isBuy) throws URISyntaxException, IOException, InterruptedException {
+    public static void placeOrder(boolean isBuy) throws IOException {
         String side = isBuy ? "buy" : "sell";
         float price, quantity;
 
@@ -42,66 +37,71 @@ public class OrderService {
             price = purchasePrice * SELL_PRICE_COEFFICIENT;
             quantity = getLastTradeHistory().getFloat("quantity");
         }
-
-        URIBuilder orderUri = new URIBuilder(HITBTC_ORDER_URL);
-        orderUri.addParameter("symbol", CURRENCY_PAIR);
-        orderUri.addParameter("side", side);
-        orderUri.addParameter("price", String.valueOf(price));
-        orderUri.addParameter("quantity", String.valueOf(quantity));
-
-        request = HttpRequest.newBuilder()
-                .POST(HttpRequest.BodyPublishers.noBody())
-                .uri(orderUri.build())
-                .header("Authorization", getAuthHeader())
-                .build();
-        int statusCode = getResponse(request).statusCode();
-        checkResponseStatusCode(statusCode);
+        System.out.println("THINGS " + side + " " + price + " " + quantity);
+        createOrder(side, price, quantity);
         log.info("Placed order - side: {}, price: {}, quantity: {}", side, price, quantity);
     }
 
-    public static void placeAdditionalBuyOrder() throws URISyntaxException, IOException, InterruptedException {
+    public static void placeAdditionalBuyOrder() throws IOException {
         JSONObject order = getLastActiveOrder();
         float quantity = order.getFloat("price") * order.getFloat("quantity");
         float price = parseFloat(String.valueOf(getAveragePrice() * BUY_PRICE_COEFFICIENT));
         String side = "buy";
-        URIBuilder orderUri = new URIBuilder(HITBTC_ORDER_URL);
-        orderUri.addParameter("symbol", CURRENCY_PAIR);
-        orderUri.addParameter("side", side);
-        orderUri.addParameter("price", String.valueOf(price));
-        orderUri.addParameter("quantity", String.valueOf(quantity));
-
-        request = HttpRequest.newBuilder()
-                .POST(HttpRequest.BodyPublishers.noBody())
-                .uri(orderUri.build())
-                .header("Authorization", getAuthHeader())
-                .build();
-        int statusCode = getResponse(request).statusCode();
-        checkResponseStatusCode(statusCode);
+        createOrder(side, price, quantity);
         log.info("Placed additional buy order - price: {}, quantity: {}", price, quantity);
     }
 
-    public static void cancelOrder(JSONObject order) throws IOException, InterruptedException {
-        String clientOrderId = order.getString("client_order_id");
-        request = HttpRequest.newBuilder()
-                .DELETE()
-                .uri(URI.create(HITBTC_ORDER_URL + "/" + clientOrderId))
+    private static void createOrder(String side, float price, float quantity) throws IOException {
+        HttpUrl.Builder orderUri = HttpUrl.parse(HITBTC_ORDER_URL).newBuilder();
+        String orderUrl = orderUri.build().toString();
+
+        RequestBody formBody = new FormBody.Builder()
+                .add("symbol", CURRENCY_PAIR)
+                .add("side", side)
+                .add("price", String.valueOf(price))
+                .add("quantity", String.valueOf(quantity))
+                .build();
+
+        request = new Request.Builder()
+                .url(orderUrl)
+                .post(formBody)
                 .header("Authorization", getAuthHeader())
                 .build();
-        int statusCode = getResponse(request).statusCode();
-        checkResponseStatusCode(statusCode);
+        Call call = client.newCall(request);
+        Response response = call.execute();
+        checkResponseStatusCode(response);
+        response.close();
+    }
+
+    public static void cancelOrder(JSONObject order) throws IOException {
+        String clientOrderId = order.getString("client_order_id");
+        request = new Request.Builder()
+                .url(HITBTC_ORDER_URL + "/" + clientOrderId)
+                .delete()
+                .header("Authorization", getAuthHeader())
+                .build();
+        Call call = client.newCall(request);
+        Response response = call.execute();
+        checkResponseStatusCode(response);
+        response.close();
         log.info("Cancel order with id " + clientOrderId);
     }
 
-    public static JSONArray getBalance() throws IOException, InterruptedException {
-        request = HttpRequest.newBuilder()
-                .GET()
-                .uri(URI.create(HITBTC_BALANCE_URL))
+    public static JSONArray getBalance() throws IOException {
+        request = new Request.Builder()
+                .url(HITBTC_BALANCE_URL)
+                .get()
                 .header("Authorization", getAuthHeader())
                 .build();
-        return new JSONArray(getResponse(request).body());
+        Call call = client.newCall(request);
+        Response response = call.execute();
+        checkResponseStatusCode(response);
+        JSONArray balance = new JSONArray(response.body().string());
+        response.close();
+        return balance;
     }
 
-    private static float getCurrencyBalance(Currencies currency) throws IOException, InterruptedException {
+    private static float getCurrencyBalance(Currencies currency) throws IOException {
         JSONArray responseBody = getBalance();
         for (int i = 0; i < responseBody.length(); i++) {
             JSONObject currencyBalance = responseBody.getJSONObject(i);
@@ -110,45 +110,56 @@ public class OrderService {
         } return 0.0f;
     }
 
-    public static JSONArray getActiveOrders() throws IOException, InterruptedException {
-        request = HttpRequest.newBuilder()
-                .GET()
-                .uri(URI.create(HITBTC_ORDER_URL))
+    public static JSONArray getActiveOrders() throws IOException {
+        request = new Request.Builder()
+                .url(HITBTC_ORDER_URL)
+                .get()
                 .header("Authorization", getAuthHeader())
                 .build();
-        HttpResponse<String> response = getResponse(request);
-        checkResponseStatusCode(response.statusCode());
-        return new JSONArray(response.body());
+        Call call = client.newCall(request);
+        Response response = call.execute();
+        checkResponseStatusCode(response);
+        JSONArray activeOrders = new JSONArray(response.body().string());
+        response.close();
+        return activeOrders;
     }
 
-    public static JSONObject getLastActiveOrder() throws IOException, InterruptedException {
-        return getActiveOrders().getJSONObject(0);
+    public static JSONObject getLastActiveOrder() throws IOException {
+        JSONArray actOrders = getActiveOrders();
+        int orderNumber = actOrders.length()-1;
+        return actOrders.getJSONObject(orderNumber);
     }
 
-    public static JSONArray getTradesHistory() throws IOException, InterruptedException {
-        request = HttpRequest.newBuilder()
-                .GET()
-                .uri(URI.create(HITBTC_TRADE_URL))
+    public static JSONArray getTradesHistory() throws IOException {
+        request = new Request.Builder()
+                .url(HITBTC_TRADE_URL)
+                .get()
                 .header("Authorization", getAuthHeader())
                 .build();
-        HttpResponse<String> response = getResponse(request);
-        checkResponseStatusCode(response.statusCode());
-        return new JSONArray(response.body());
+        Call call = client.newCall(request);
+        Response response = call.execute();
+        checkResponseStatusCode(response);
+        JSONArray tradesHistory = new JSONArray(response.body().string());
+        response.close();
+        return tradesHistory;
     }
 
-    public static JSONObject getLastTradeHistory() throws IOException, InterruptedException {
+    public static JSONObject getLastTradeHistory() throws IOException {
         return getTradesHistory().getJSONObject(0);
     }
 
-    public static float getAveragePrice() throws IOException, InterruptedException {
+    public static float getAveragePrice() throws IOException {
         String url = "https://api.binance.com/api/v3/avgPrice?symbol=" + CURRENCY_PAIR;
-        request = HttpRequest.newBuilder()
-                .GET()
-                .uri(URI.create(url))
+        request = new Request.Builder()
+                .url(url)
+                .get()
                 .build();
-        HttpResponse<String> response = getResponse(request);
-        checkResponseStatusCode(response.statusCode());
-        String priceValue = new JSONObject(response.body()).getString("price");
+        Call call = client.newCall(request);
+        Response response = call.execute();
+        checkResponseStatusCode(response);
+
+        String priceValue = new JSONObject(response.body().string()).getString("price");
+        response.close();
         return parseFloat(priceValue);
     }
 
@@ -164,10 +175,6 @@ public class OrderService {
         }
     }
 
-    private static HttpResponse<String> getResponse(HttpRequest request) throws IOException, InterruptedException {
-        return client.send(request, HttpResponse.BodyHandlers.ofString());
-    }
-
     private static String getAuthHeader() {
         final String API_KEY = "48AEDmQHNHG8WP4-WQ0d83KCf0U-804J";
         final String SECRET_KEY = "1y4wmLtPB_ZaJpwrJ_o54GAvB7eUngEL";
@@ -176,9 +183,10 @@ public class OrderService {
                 .encodeToString(valueToEncode.getBytes());
     }
 
-    private static void checkResponseStatusCode(int statusCode) {
+    private static void checkResponseStatusCode(Response response) {
+        int statusCode = response.code();
         if (statusCode != 200 && statusCode != 201) {
-            log.info("Exception with status code " + statusCode);
+            log.info("Exception code: {}, message: {}", statusCode, response.body());
             throw new RuntimeException();
         }
     }
